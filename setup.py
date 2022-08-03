a1baebaae64ac8788b297f2044ce14ec74ee4a6f
Pavan Kumar Adapa <65138800+pavanadapa@users.noreply.github.com>
2022-08-02 21:56:35 -0400

    saved from vsc

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup
 
 import versioneer
 
-cmdclass = versioneer.get_cmdclass()
-
 if __name__ == "__main__":
     # Freeze to support parallel compilation when using spawn instead of fork
     setup(
         version=versioneer.get_version(),
-        cmdclass=cmdclass,
+        cmdclass=versioneer.get_cmdclass(),
     )
